afecb67cd78442524d48e8cac91812bc3964ea7a
Asif Alam <asif.k.alam@net.usek.edu.lb>
2024-01-25 19:56:11 +0200

    Delete unused assets and update portfolio link

--- script.js
+++ script.js
@@ -39,3 +39,60 @@ upbtn.addEventListener("click", () => {
 		showMoreBtn.style.display = "block";
 	}
 });
+
+const about = document.getElementById("about");
+const projects = document.getElementById("projects");
+const contact = document.getElementById("contact");
+const experience = document.getElementById("experience");
+const Logo = document.getElementById("logo");
+
+const aboutLink = document.getElementById("about-link");
+const projectsLink = document.getElementById("projects-link");
+const contactLink = document.getElementById("contact-link");
+const experienceLink = document.getElementById("experience-link");
+
+window.addEventListener("scroll", () => {
+	if (window.scrollY < about.offsetTop) {
+		Logo.classList.add("active");
+		aboutLink.classList.remove("active");
+		experienceLink.classList.remove("active");
+		projectsLink.classList.remove("active");
+		contactLink.classList.remove("active");
+	} else if (
+		window.scrollY >= about.offsetTop &&
+		window.scrollY < experience.offsetTop
+	) {
+		aboutLink.classList.add("active");
+		Logo.classList.remove("active");
+		experienceLink.classList.remove("active");
+		projectsLink.classList.remove("active");
+		contactLink.classList.remove("active");
+	} else if (
+		window.scrollY >= experience.offsetTop &&
+		window.scrollY < projects.offsetTop
+	) {
+		aboutLink.classList.remove("active");
+		experienceLink.classList.add("active");
+		Logo.classList.remove("active");
+		projectsLink.classList.remove("active");
+		contactLink.classList.remove("active");
+	} else if (
+		window.scrollY >= projects.offsetTop &&
+		window.scrollY < contact.offsetTop - 400
+	) {
+		console.log(projects.offsetTop);
+		console.log(window.scrollY);
+		console.log(contact.offsetTop);
+		experienceLink.classList.remove("active");
+		aboutLink.classList.remove("active");
+		projectsLink.classList.add("active");
+		Logo.classList.remove("active");
+		contactLink.classList.remove("active");
+	} else {
+		aboutLink.classList.remove("active");
+		projectsLink.classList.remove("active");
+		experienceLink.classList.remove("active");
+		Logo.classList.remove("active");
+		contactLink.classList.add("active");
+	}
+});
